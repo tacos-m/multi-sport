e00afae28fe7ce774b3ff2ddbd959e75f0e8da9d
ATTAR SELMA <selma@macbook-air-de-attar.home>
2026-05-03 18:02:19 +0200

    résolution conflit

--- Script/Plongée.js
+++ Script/Plongée.js
@@ -1,6 +1,7 @@
 //  POPUP
 function openPopup(type) {
     let text = "";
+    let showListe = false;
 
     if (type === "materiel") {
         text = "Le matériel de plongée comprend le masque, les palmes, la combinaison, la bouteille et le détendeur.";
@@ -8,6 +9,7 @@ function openPopup(type) {
 
     if (type === "lieux") {
         text = "On peut plonger en mer, dans les océans, les récifs coralliens ou même dans certains lacs.";
+        showListe = true;
     }
 
     if (type === "bienfaits") {
@@ -19,22 +21,41 @@ function openPopup(type) {
     }
 
     document.getElementById("popupText").innerText = text;
+
+    //  affiche ou cache la liste seulement pour lieux
+    document.getElementById("listeEspeces").style.display =
+        showListe ? "block" : "none";
+
     document.getElementById("popup").style.display = "block";
 }
 
-//  FERMER POPUP
+// fermer popup
 function closePopup() {
     document.getElementById("popup").style.display = "none";
 }
 
 
 //  QUIZ
-function checkAnswer(answer) {
+function checkAnswer(button, isCorrect) {
     let result = document.getElementById("result");
 
-    if (answer === "b" || answer === "d") {
+    // reset couleur des boutons du même groupe
+    let parent = button.parentElement;
+    let buttons = parent.querySelectorAll("button");
+
+    buttons.forEach(btn => {
+        btn.style.backgroundColor = "";
+        btn.style.color = "";
+    });
+
+    if (isCorrect) {
+        button.style.backgroundColor = "green";
+        button.style.color = "white";
         result.innerText = "✅ Bonne réponse !";
     } else {
+        button.style.backgroundColor = "red";
+        button.style.color = "white";
         result.innerText = "❌ Mauvaise réponse";
     }
 }
+
